f4500936a19b319adf2bf0429b929e6f9463458f
Ebikeme Ese <ebikemeese257@gmail.com>
2026-02-15 22:22:43 +0100

    update settings.py
    Co-Authored-By: Tobechi <88856392+Tobechi15@users.noreply.github.com>

--- django_rest_prj/settings.py
+++ django_rest_prj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom apps
     'rest_framework',
     'django_filters',
     'students',
